1407622f388618da794e593be9ece581774178a9
ilariamotta <ilaria.motta93@gmail.com>
2025-11-03 15:07:02 +0100

    feat: costruzione logica primo esercizio

---
+++ js bonus/script.js
@@ -0,0 +1,4 @@
+// Bonus:
+// Come creare una funzione che dato una frase si capitalizzano tutte le parole di questa frase
+
+// "pippo studia sviluppo web" --->  "Pippo Studia Sviluppo Web"
---
+++ js palidroma/script.js
@@ -0,0 +1,13 @@
+// PALIDROMA
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma (NON usare split, reverse e join)
+
+
+// funzione che all'immissione di una parola controlla, scorrendo, se la parola è palindroma
+    //1.creazione funzione
+    //2. creazione ciclo for che scorre le lettere e guarda se si possono invertire
+    //3. SE sì, allora TRUE
+    //4. ALTRIMENTI FALSE
+// nome funzione: isPali
+// param: parola (stringa)
+// return: boolean
