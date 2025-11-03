46803eb59a598efd2b4ff3c4596e860796c92478
ilariamotta <ilaria.motta93@gmail.com>
2025-11-03 16:50:21 +0100

    feat: else if e else pariedispari

--- j paridispari/script.js
+++ j paridispari/script.js
@@ -7,29 +7,31 @@
 // Dichiariamo chi ha vinto.
 
 // funzione che ci dice chi ha vinto il gioco
-    //1.creazione funzione
-    //2.chiediamo all'utente se pari o dispari
-    //3.generazione numero random sia per utente che computer
-    //4.somma dei due numeri usciti
-    //5.SE somma corrisponde alla scelta dell'utente ALLORA vince, ALTRIMENTI vince computer
+//1.creazione funzione
+//2.chiediamo all'utente se pari o dispari
+//3.generazione numero random sia per utente che computer
+//4.somma dei due numeri usciti
+//5.SE somma corrisponde alla scelta dell'utente ALLORA vince, ALTRIMENTI vince computer
 
 // nome funzione: whoWins
 // param: scelta utente e la somma
 // return: chi ha vinto (stringa)
 
 
-function whoWins (sceltaUtente, somma) {
-if (somma % 2 === 0 && sceltaUtente === "pari") {
-   return "Hai vinto!"
-
-
-}
+function whoWins(sceltaUtente, somma) {
+    if (somma % 2 === 0 && sceltaUtente === "pari") {
+        return "Hai vinto!";
+    } else if (somma % 2 !== 0 && sceltaUtente === "dispari") {
+        return "Hai vinto!";
+    } else {
+        return "Computer ha vinto";
+    }
 
 }
 
 sceltaUtente = prompt("Pari o dispari?").toLowerCase()
 numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"))
-numeroPc = Math.floor(Math.random() * 5 ) + 1;
+numeroPc = Math.floor(Math.random() * 5) + 1;
 somma = numeroUtente + numeroPc;
 
 // whoWins("pari", 8);
