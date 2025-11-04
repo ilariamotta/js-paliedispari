a4029f18b770ea6a683997d9d0110a792c183543
ilariamotta <ilaria.motta93@gmail.com>
2025-11-04 18:13:18 +0100

    feat: bonus completato

--- js bonus/script.js
+++ js bonus/script.js
@@ -2,3 +2,17 @@
 // Come creare una funzione che dato una frase si capitalizzano tutte le parole di questa frase
 
 // "pippo studia sviluppo web" --->  "Pippo Studia Sviluppo Web"
+
+const fraseIniziale = "pippo studia sviluppo web";
+
+
+function toCap(frase) {
+    const array = frase.split(" ");
+    for (let i = 0; i < array.length; i++) {
+    array[i] = array[i].slice(0, 1).toUpperCase() + array[i].slice(1, array[i].length);
+    }
+    newString = array.join(" ");
+    return newString;
+}
+
+console.log(toCap(fraseIniziale));
